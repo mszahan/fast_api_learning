21fe91c1eb7dc7e7c1a302464d520e66785914dd
mszahan <mszahan1@gmail.com>
2023-04-02 23:41:48 +0600

    used the basemodel for more robust api

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
 
 @app.get("/")
 def home():
     return {'Hello': 'World'}
 
 @app.get("/items/{item_id}")
-def item(item_id:int, q:Union[str, None] = None):
-    return {"Item Id": item_id, "query":q}
+def read_item(item_id:int, q:Union[str, None] = None):
+    return {"item_id": item_id, "query":q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int, item:Item):
+    return {'item_name':item.name, 'item_price':item.price, 'item_id':item_id}
