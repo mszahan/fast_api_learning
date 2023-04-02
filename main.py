ab58d561f312b00adc5dc690cdda403da6511c60
mszahan <mszahan1@gmail.com>
2023-04-02 23:06:48 +0600

    added new GET endpoint which takes query

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {'Hello': 'World'}
+
+@app.get("/items/{item_id}")
+def item(item_id:int, q:Union[str, None] = None):
+    return {"Item Id": item_id, "query":q}
