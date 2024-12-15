759d986f64cfb36a0ca5e63de4fde0bdb72729e1
mszahan <mszahan1@gmail.com>
2024-12-15 19:33:11 +0600

    added schema_extra still not showing to the documentation

--- web_api_packt/todos/model.py
+++ web_api_packt/todos/model.py
@@ -4,3 +4,11 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+    ## example schema to show in documentation
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "This is an example schema"
+            }
+        }
