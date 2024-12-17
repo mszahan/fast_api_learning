2a170923a7405f4d53e3d94b12fd314eb499c2eb
mszahan <mszahan1@gmail.com>
2024-12-17 23:30:36 +0600

    added routers for all the events endpoints

--- web_api_packt/planner/main.py
+++ web_api_packt/planner/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.events import event_router
 
 import uvicorn
 
@@ -7,6 +8,7 @@ app = FastAPI()
 
 ## register routes
 app.include_router(user_router, prefix='/user')
+app.include_router(event_router, prefix='/event')
 
 
 if __name__ == '__main__':
