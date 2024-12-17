59b1ef4a4c9c0b30138b724bef0053652fac4d4b
mszahan <mszahan1@gmail.com>
2024-12-17 19:57:57 +0600

    created folder structure and build model for user and event

---
+++ web_api_packt/planner/models/events.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+
+class Event(BaseModel):
+    id: int
+    title: str
+    image: str
+    description: str
+    tags: List [str]
+    location: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastApi book Launch",
+                "image": "https://image.com/image.png",
+                "description": "the even will be on launching the new book",
+                "tags": ["python", "fasapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
