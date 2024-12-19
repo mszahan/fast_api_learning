062e016b2682091b964384e0101a8cde28eb9a4d
mszahan <mszahan1@gmail.com>
2024-12-19 15:02:48 +0600

    connected sqlite database

--- web_api_packt/planner/main.py
+++ web_api_packt/planner/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from database.connection import conn
+
 from routes.users import user_router
 from routes.events import event_router
 
 import uvicorn
+
 
 app = FastAPI()
 
 ## register routes
 app.include_router(user_router, prefix='/user')
 app.include_router(event_router, prefix='/event')
+
+
+@app.on_event('startup')
+def on_startup():
+    conn()
+
+@app.get('/')
+async def home():
+    return RedirectResponse(url='/event/')
 
 
 if __name__ == '__main__':
