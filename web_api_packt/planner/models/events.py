f7f508b3037d1706ec4f0bded9ca4ac334963db1
mszahan <mszahan1@gmail.com>
2024-12-19 12:20:06 +0600

    updated event model with sql model

--- web_api_packt/planner/models/events.py
+++ web_api_packt/planner/models/events.py
@@ -1,13 +1,35 @@
-from pydantic import BaseModel
-from typing import List
+# from pydantic import BaseModel
+from typing import List, Optional
+from sqlmodel import JSON, Field, Column, SQLModel
 
-class Event(BaseModel):
-    id: int
+
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List [str]
+    tags: List [str] = Field(sa_column=Column(JSON))
     location: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_schema_extra = {
+            "example": {
+                "title": "FastApi book Launch",
+                "image": "https://image.com/image.png",
+                "description": "the even will be on launching the new book",
+                "tags": ["python", "fasapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         json_schema_extra = {
