47028ea6dd4d282245ad457adaf6075df3ed1c6a
mszahan <mszahan1@gmail.com>
2024-12-21 14:37:07 +0600

    added env file and added two models to mongo connection

--- web_api_packt/planner/database/connection.py
+++ web_api_packt/planner/database/connection.py
@@ -3,6 +3,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional
 from pydantic import BaseSettings
 
+from models.events import Event
+from models.users import User
+
+
 
 class Settings(BaseSettings):
     DATABASE_URL: Optional [str] = None
@@ -10,7 +14,7 @@ class Settings(BaseSettings):
     async def initialize_database(self):
         client = AsyncIOMotorClient(self.DATABASE_URL)
         await init_beanie(database=client.get_default_database,
-                          document_models=[])
+                          document_models=[Event, User])
         
     class Config:
         env_file = '.env'
