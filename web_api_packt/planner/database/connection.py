165b56713c6e27721a32600ccd3344408a1e4346
mszahan <mszahan1@gmail.com>
2024-12-22 11:57:25 +0600

    updated event route to interact with mongodb

--- web_api_packt/planner/database/connection.py
+++ web_api_packt/planner/database/connection.py
@@ -1,7 +1,9 @@
 from beanie import init_beanie, PydanticObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional, List, Any
-from pydantic import BaseSettings, BaseModel
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
 
 from models.events import Event
 from models.users import User
