44edb214e43016c9df65f72e1573f5684106ed84
mszahan <mszahan1@gmail.com>
2025-02-06 23:54:53 +0600

    created custom serialization with field_serializer

---
+++ farm_packt/type_hits/custom_serializer_ch3_13.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from pydantic import BaseModel, field_serializer
+
+
+class Account(BaseModel):
+    balance: float
+    updated: datetime
+
+    @field_serializer('balance', when_used='always')
+    def serialize_balance(self, value: float) -> float:
+        return round(value, 2)
+    
+    @field_serializer('updated', when_used='json')
+    def serialize_updated(self, value:datetime) -> str:
+        return value.isoformat()
+
+
+account_data = {
+    'balance': 123.45545,
+    'updated': datetime.now(),
+}
+
+account = Account.model_validate(account_data)
+
+print('Ditionary: ', account.model_dump())
+print('json: ', account.model_dump_json())
