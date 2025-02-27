409a83f1b65271f2dbb5902ebc2df8f1b915f61c
mszahan <mszahan1@gmail.com>
2025-02-27 22:21:19 +0600

    added dependency injection

---
+++ farm_packt/fast_ch4/dependency_injection_ch4_15.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+async def pagination(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {'q': q, 'skip': skip, 'limit': limit}
+
+
+@app.get('/cars')
+async def read_items(commons: Annotated[dict, Depends(pagination)]):
+    return commons
+
+
+@app.get('/users')
+async def read_users(commons: Annotated[dict, Depends(pagination)]):
+    return commons
