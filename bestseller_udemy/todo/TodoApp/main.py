a7ec8d856967664f284557cd8f221ae4c751a410
mszahan <mszahan1@gmail.com>
2025-05-12 21:44:51 +0600

    added todo detail endpoint with proper statsus and httpexception

--- bestseller_udemy/todo/TodoApp/main.py
+++ bestseller_udemy/todo/TodoApp/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
 from typing import Annotated
 from sqlalchemy.orm import Session
 from . import models
@@ -19,6 +20,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK)
 async def todo_list(db: db_dependency):
     return db.query(Todo).all()
+
+
+@app.get('/todo/{todo_id}')
+async def todo_detail(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first() # first stops further filtering when the first id is found
+    if todo is not None:
+        return todo
+    raise HTTPException(status_code=404, detail='Todo not found')
