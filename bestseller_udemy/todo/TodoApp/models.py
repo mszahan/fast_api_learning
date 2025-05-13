83a6776824b12524cc8910e7bf31cebdf4d05535
mszahan <mszahan1@gmail.com>
2025-05-13 20:12:07 +0600

    created model for user database table

--- bestseller_udemy/todo/TodoApp/models.py
+++ bestseller_udemy/todo/TodoApp/models.py
@@ -1,6 +1,18 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
+
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 class Todo(Base):
     __tablename__ = 'todo'
@@ -10,3 +22,4 @@ class Todo(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('user.id'))
