2cde70008d91da194275b6cb381309656918582f
mszahan <mszahan1@gmail.com>
2025-05-18 18:01:16 +0600

    configured the app to use postgresql

--- bestseller_udemy/todo/TodoApp/database.py
+++ bestseller_udemy/todo/TodoApp/database.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'sqlite:///./todoapp.db'
+#for sqlite3
+# DATABASE_URL = 'sqlite:///./todoapp.db'
+# engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+#for postgresql
+DATABASE_URL = 'postgresql://postgres:1294@localhost/tododb'
+engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
