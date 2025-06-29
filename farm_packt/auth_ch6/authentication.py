06a154de96c74b9c996c0711b035173d84d982f4
mszahan <mszahan1@gmail.com>
2025-06-29 21:04:41 +0600

    added gitignore and formatted code with autopep8

--- farm_packt/auth_ch6/authentication.py
+++ farm_packt/auth_ch6/authentication.py
@@ -1,7 +1,7 @@
-import datetime 
+import datetime
 import jwt
 from fastapi import HTTPException, Security
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer 
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from passlib.context import CryptContext
 
 
@@ -12,10 +12,10 @@ class AuthHandler:
 
     def get_password_hash(self, password: str) -> str:
         return self.pwd_context.hash(password)
-    
+
     def verify_password(self, plain_password: str, hashed_password: str) -> bool:
         return self.pwd_context.verify(plain_password, hashed_password)
-    
+
     def encode_token(self, user_id: int, username: str) -> str:
         payload = {
             'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=30),
@@ -24,13 +24,14 @@ class AuthHandler:
 
         }
         return jwt.encode(payload, self.secret, algorithm='HS256')
-    
+
     def decode_toke(self, token: str):
         try:
             payload = jwt.decode(token, self.secret, algorithms='HS256')
             return payload['sub']
         except jwt.ExpiredSignatureError:
-            raise HTTPException(status_code=401, detail='Signature has expired')
+            raise HTTPException(
+                status_code=401, detail='Signature has expired')
         except jwt.InvalidTokenError:
             raise HTTPException(status_code=401, detail='Invalid token')
 
