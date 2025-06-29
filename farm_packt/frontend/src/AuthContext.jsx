1fe400efc6456c4bf1199aa83a0851bd4a338864
mszahan <mszahan1@gmail.com>
2025-06-29 23:40:14 +0600

    created AuthContext for register, login and logout

---
+++ farm_packt/frontend/src/AuthContext.jsx
@@ -0,0 +1,83 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [jwt, setJwt] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const register = async (username, password) => {
+    try {
+      const resposen = await fetch("http://localhost:8000/ursers/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+      if (resposen.ok) {
+        const data = await resposen.json();
+        setMessage(`Registration successful: user ${data.username}`);
+      } else {
+        const data = await resposen.json();
+        setMessage(`Registration failed: ${JSON.stringify(data)}`);
+      }
+    } catch (error) {
+      setMessage(`Registration failed: ${JSON.stringify(error)}`);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setJwt(null);
+    setMessage("Logout successful");
+  };
+
+  const login = async (username, password) => {
+    setJwt(null);
+    const response = await fetch("http://localhost:8000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      setJwt(data.token);
+      setUser({ username });
+      setMessage(
+        `login successful : token ${data.token.slice(
+          0,
+          10
+        )}... user ${username}`
+      );
+    } else {
+      const data = await response.json();
+      setMessage("login failed" + data.detail);
+      setUser({ username: null });
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        jwt,
+        register,
+        login,
+        logout,
+        message,
+        setMessage,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
