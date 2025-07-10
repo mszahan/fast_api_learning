d4c9e1ec2aa5ab916ce6b82a7d7082629988f27c
mszahan <mszahan1@gmail.com>
2025-07-10 23:35:25 +0600

    changed the app of fastapi

--- farm_packt/car_backend/app.py
+++ farm_packt/car_backend/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from motor import motor_asyncio
 from config import BaseConfig
+
 from routers.cars import router as cars_router
 from routers.users import router as users_router
 
@@ -22,6 +24,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 app.include_router(cars_router, prefix='/cars', tags=['cars'])
 app.include_router(users_router, prefix='/users', tags=['users'])
 
