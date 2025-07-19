c418667a5db357fa4a6273b1759a1116f5927a65
mszahan <mszahan1@gmail.com>
2025-07-19 23:33:35 +0600

    added error page

---
+++ farm_packt/next-frontend/src/app/cars/error.js
@@ -0,0 +1,10 @@
+"use client";
+const error = () => {
+  return (
+    <div className="bg-red-800 text-white p-3">
+      There was an error fetching the data. Please try again later.
+    </div>
+  );
+};
+
+export default error;
