93d7c6142ad553f103070ae0135045aa7214a47f
mszahan <mszahan1@gmail.com>
2025-07-19 23:17:37 +0600

    added car list page

--- farm_packt/next-frontend/src/app/cars/page.js
+++ farm_packt/next-frontend/src/app/cars/page.js
@@ -1,9 +1,25 @@
-const Cars = () => {
+import Link from "next/link";
+
+const Cars = async () => {
+  const data = await fetch(`${process.env.API_URL}/cars/`, {
+    next: { revalidate: 10 },
+  });
+
+  const cars = await data.json();
   return (
     <div className="p-5 m-5">
-      <h1 className="text-2xl font-bold">
-        Welcome to Car list page of my app App
-      </h1>
+      <h1 className="text-2xl font-bold">Cars List</h1>
+      <div>
+        {cars.map((car) => (
+          <div key={car._id} className="m-4 bg-white p-2">
+            <Link href={`/cars/${car._id}`}>
+              <p>
+                {car.brand} {car.make} from {car.year}
+              </p>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
