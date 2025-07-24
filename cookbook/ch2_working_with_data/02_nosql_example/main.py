56db0722433d9e54d0f6315cb4196e269f6400e2
mszahan <mszahan1@gmail.com>
2025-07-24 22:43:09 +0600

    added advanced pydantic validation to user model with age

--- cookbook/ch2_working_with_data/02_nosql_example/main.py
+++ cookbook/ch2_working_with_data/02_nosql_example/main.py
@@ -1,15 +1,29 @@
-from database import user_collection
+from typing import Optional
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
 from bson import ObjectId
+from database import user_collection
 
 
 app = FastAPI()
 
 
+class Tweet(BaseModel):
+    content: str
+    hashtags: list[str]
+
+
 class User(BaseModel):
     name: str
-    email: str
+    email: EmailStr
+    age: int
+    tweets: list[Tweet] | None = None
+
+    @field_validator('age')
+    def validate_age(cls, value):
+        if value < 18 or value > 100:
+            raise ValueError('Age must be between 18 and 100')
+        return value
 
 
 class UserResponse(User):
@@ -30,10 +44,11 @@ def create_user(user: User):
 
 
 @app.get('/user/{user_id}')
-def read_user(user_id: str):
+def read_user(user_id: str) -> UserResponse:
     db_user = user_collection.find_one(
         {'_id': ObjectId(user_id) if ObjectId.is_valid(user_id) else None})
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    user_response = UserResponse(id=str(db_user['_id']), **db_user)
-    return user_response
+    # user_response = UserResponse(id=str(db_user['_id']), **db_user)
+    db_user['id'] = str(db_user['_id'])
+    return db_user
