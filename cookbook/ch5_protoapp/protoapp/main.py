fb2393475e24d171db02ea2740765dddb0708cba
mszahan <mszahan1@gmail.com>
2025-07-31 00:14:10 +0600

    did unit test with memory database and fixture

--- cookbook/ch5_protoapp/protoapp/main.py
+++ cookbook/ch5_protoapp/protoapp/main.py
@@ -1,4 +1,20 @@
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException, Request, status
+from sqlalchemy.orm import Session
+from protoapp.database import SessionLocal, Item
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class ItemSchema(BaseModel):
+    name: str
+    color: str
 
 
 app = FastAPI()
@@ -7,3 +23,20 @@ app = FastAPI()
 @app.get('/home')
 async def read_main():
     return {'message': 'Hello World'}
+
+
+@app.post('/item/', response_model=int, status_code=status.HTTP_201_CREATED)
+async def add_item(item: ItemSchema, db: Session = Depends(get_db)):
+    db_item = Item(name=item.name, color=item.color)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item.id
+
+
+@app.get('/item/{item_id}', response_model=ItemSchema)
+async def get_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail='Item not found')
+    return item
--- cookbook/ch5_protoapp/tests/conftest.py
+++ cookbook/ch5_protoapp/tests/conftest.py
@@ -1,10 +1,38 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
-from protoapp.main import app
+from protoapp.database import Base
+from protoapp.main import app, get_db
+
+
+engine = create_engine(
+    'sqlite:///:memory:',
+    connect_args={'check_same_thread': False},
+    poolclass=StaticPool,
+)
+
+Base.metadata.create_all(engine)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def test_db_session():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @pytest.fixture(scope='function')
-def test_client():
+def test_client(test_db_session):
     client = TestClient(app)
-    yield client
+    app.dependency_overrides[get_db] = (
+        lambda: test_db_session
+    )
+    return client
