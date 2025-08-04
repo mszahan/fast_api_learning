bfaf9bedf42ffa8cc1efd0c0ef983b564caf4bf7
mszahan <mszahan1@gmail.com>
2025-08-04 17:14:18 +0600

    added all the operations for event, sponsor, and sponsorships

--- cookbook/ch6_ticketing_system/app/operations.py
+++ cookbook/ch6_ticketing_system/app/operations.py
@@ -1,7 +1,9 @@
+from certifi import where
 from sqlalchemy import select, update, delete, text, and_
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.database import Ticket, TicketDetails
+from sqlalchemy.orm import joinedload, load_only
+from app.database import Ticket, TicketDetails, Event, Sponsor, Sponsorship
 
 
 async def create_ticket(
@@ -13,7 +15,8 @@ async def create_ticket(
     ticket = Ticket(
         show=show_name,
         user=user,
-        price=price
+        price=price,
+        details=TicketDetails()
     )
     async with db_session.begin():
         db_session.add(ticket)
@@ -87,7 +90,7 @@ async def update_ticket_details(
     ticket_query = update(TicketDetails).where(
         TicketDetails.ticket_id == ticket_id
     )
-    if updating_ticket_details != {}:
+    if updating_ticket_details == {}:
         return False
     ticket_query = ticket_query.values(**updating_ticket_details)
     async with db_session as session:
@@ -110,3 +113,126 @@ async def delete_ticket(
         if tickets_removed.rowcount == 0:
             return False
         return True
+
+
+async def creat_event(
+        db_session: AsyncSession,
+        event_name: str,
+        nb_tickets: int | None = 0,
+) -> int:
+    async with db_session.begin():
+        event = Event(name=event_name)
+        db_session.add(event)
+        await db_session.flush()
+        event_id = event.id
+        tickets = [Ticket(show=event_name,
+                          details=TicketDetails(seat=f'{n}A'),
+                          event_id=event_id)
+                   for n in range(nb_tickets)]
+        db_session.add_all(tickets)
+        await db_session.commit()
+    return event_id
+
+
+async def create_sponsor(
+        db_session: AsyncSession,
+        sponsor_name: str
+) -> int:
+    async with db_session.begin():
+        sponsor = Sponsor(name=sponsor_name)
+        db_session.add(sponsor)
+        try:
+            await db_session.flush()
+        except IntegrityError:
+            return
+        sponsor_id = sponsor.id
+        await db_session.commit()
+    return sponsor_id
+
+
+async def add_sponsor_to_event(
+        db_session: AsyncSession,
+        event_id: int,
+        sponsor_id: int,
+        amount: float
+) -> bool:
+    query = text(
+        'INSERT INTO sponsorships'
+        '(event_id, sponsor_id, amount)'
+        'VALUES(:event_id, :sponsor_id, :amount)'
+        'ON CONFLICT (event_id, sponsor_id)'
+        'DO UPDATE SET amount = '
+        'sponsorships.amount + EXCLUDED.amount'
+    )
+    params = {
+        'event_id': event_id,
+        'sponsor_id': sponsor_id,
+        'amount': amount
+    }
+    async with db_session.begin():
+        result = await db_session.execute(query, params)
+        await db_session.commit()
+        if result.rowcount == 0:
+            return False
+    return True
+
+
+async def get_event(
+        db_session: AsyncSession, event_id: int
+) -> Event | None:
+    query = select(Event).where(Event.id == event_id).options(
+        joinedload(Event.sponsors))
+    async with db_session as session:
+        result = await session.execute(query)
+        event = result.scalars().first()
+    return event
+
+
+async def get_events_with_sponsors(
+        db_session: AsyncSession) -> list[Event]:
+    query = select(Event).options(
+        joinedload(Event.sponsors)
+    )
+    async with db_session as session:
+        result = await session.execute(query)
+        events = result.scalars().all()
+    return events
+
+
+async def get_event_sponsorships_with_amount(
+        db_session: AsyncSession, event_id: int
+):
+    query = select(Sponsor.name, Sponsorship.amount).join(
+        Sponsorship, Sponsorship.sponsor_id == Sponsor.id,
+    ).where(Sponsorship.event_id == event_id).order_by(Sponsorship.amount.desc())
+    async with db_session as session:
+        result = await session.execute(query)
+        sponsor_contributions = result.fetchall()
+    return sponsor_contributions
+
+
+async def get_events_tickets_with_user_price(
+        db_session: AsyncSession, event_id: int
+) -> list[Ticket]:
+    query = select(Ticket).where(Ticket.event_id == event_id).options(
+        load_only(Ticket.id, Ticket.user, Ticket.price)
+    )
+    async with db_session as session:
+        result = await session.execute(query)
+        tickets = result.scalars().all()
+    return tickets
+
+
+async def sell_ticket_to_user(
+        db_session: AsyncSession, ticket_id: int, user: str
+) -> bool:
+    ticket_query = update(Ticket).where(and_(
+        Ticket.id == ticket_id, Ticket.sold == False)).values(
+        user=user, sold=True
+    )
+    async with db_session as session:
+        result = await session.execute(ticket_query)
+        await session.commit()
+        if result.rowcount == 0:
+            return False
+    return True
