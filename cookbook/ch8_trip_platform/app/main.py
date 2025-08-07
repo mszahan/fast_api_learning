0ec325b5f0dd8faeaebd345f8539dc55ffb747dc
mszahan <mszahan1@gmail.com>
2025-08-07 21:51:55 +0600

    implemented background task with fastapi BackgroundTasks

--- cookbook/ch8_trip_platform/app/main.py
+++ cookbook/ch8_trip_platform/app/main.py
@@ -1,6 +1,7 @@
+import logging
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, BackgroundTasks
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
@@ -10,6 +11,10 @@ from app.dependencies import time_range, select_category, check_coupon
 from app.middleware import ClientInfoMiddleware
 from app.profiler import ProfileEndpontMiddleware
 from app import internationalization
+from app.background_tasks import stor_query_to_external_db
+
+logger = logging.getLogger('uvicorn.error')
+
 
 app = FastAPI()
 
@@ -37,6 +42,7 @@ def get_tours(time=Depends(time_range)):
 
 @app.get('/v2/trips/{category}')
 def get_trips_by_category(
+    background_tasks: BackgroundTasks,
     category: Annotated[select_category, Depends()],
     discount: Annotated[bool, Depends(check_coupon)]
 ):
@@ -45,4 +51,11 @@ def get_trips_by_category(
 
     if discount:
         message += 'The coupon code is valid, you will get the discount'
+    background_tasks.add_task(
+        stor_query_to_external_db, message
+    )
+    logger.info(
+        'Query sent to background task,'
+        'end of request.'
+    )
     return message
