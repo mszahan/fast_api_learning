4d01a183f134ab4e7a7ffb4c6b036c753ae29d9f
mszahan <mszahan1@gmail.com>
2025-08-07 18:44:49 +0600

    tried optimizing application performance

--- cookbook/ch8_trip_platform/app/main.py
+++ cookbook/ch8_trip_platform/app/main.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from fastapi import FastAPI, Depends
 from app.dependencies import time_range, select_category, check_coupon
 from app.middleware import ClientInfoMiddleware
+from app.profiler import ProfileEndpontMiddleware
 from app import internationalization
 
 app = FastAPI()
@@ -10,6 +11,7 @@ app = FastAPI()
 app.include_router(internationalization.router)
 
 app.add_middleware(ClientInfoMiddleware)
+app.add_middleware(ProfileEndpontMiddleware)
 
 
 @app.get('/v1/trips')
---
+++ cookbook/ch8_trip_platform/app/profiler.py
@@ -0,0 +1,20 @@
+import os
+from fastapi import Request
+from pyinstrument import Profiler
+from starlette.middleware.base import BaseHTTPMiddleware
+
+profiler = Profiler(
+    interval=0.001, async_mode='enabled'
+)
+
+
+class ProfileEndpontMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if not profiler.is_running:
+            profiler.start()
+        response = await call_next(request)
+        if profiler.is_running:
+            profiler.stop()
+            profiler.write_html(os.getcwd + '/profile.html')
+            profiler.start()
+        return response
