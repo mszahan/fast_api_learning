e2f9c3e5f9beaaae7fcdd79d5ece1094667814ed
mszahan <mszahan1@gmail.com>
2025-08-14 23:14:02 +0600

    integrated fastapi with cohere

---
+++ cookbook/ch10_python_libraries/04_chef_ai/main.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Body, Request
+from handlers import generate_chat_completion
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield {'messages': []}
+
+
+app = FastAPI(title='Chef Cuisine chatbot app', lifespan=lifespan)
+
+
+@app.post('/query')
+async def query_chat_bot(request: Request,
+                         query: Annotated[str, Body(min_length=1)]) -> str:
+    answer = await generate_chat_completion(query, request.state.messages)
+    return answer
