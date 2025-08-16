3b10d0b9cfd9eaee55b2d954944fb507ee48c8fa
mszahan <mszahan1@gmail.com>
2025-08-16 19:06:45 +0600

    implemented webhook

--- cookbook/ch11_middleware/main.py
+++ cookbook/ch11_middleware/main.py
@@ -1,18 +1,26 @@
 import logging
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from contextlib import asynccontextmanager
 from starlette.middleware import Middleware
 from middleware.request_middleware import HashBodyMiddleware
 from middleware.asgi_middleware import ASGIMiddleware
 from middleware.response_middleware import HeaderResponseMiddleware
+from middleware.webhook import WebhookSenderMiddleware
 
 
 logger = logging.getLogger('uvicorn')
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield {'webhook_urls': []}
+
+
 app = FastAPI(
     title='Middleware Application',
+    lifespan=lifespan,
     middleware=[
         Middleware(ASGIMiddleware, parameter='example_parameter')
     ]
@@ -33,6 +41,7 @@ app.add_middleware(
 )
 
 app.add_middleware(TrustedHostMiddleware, allowed_hosts=['localhost'])
+app.add_middleware(WebhookSenderMiddleware)
 
 
 @app.get('/')
@@ -44,3 +53,11 @@ async def read_root():
 async def send(message: str = Body()):
     logger.info(f'Message: {message}')
     return {'message': message}
+
+
+@app.post('/register-webhook-url')
+async def add_webhook_url(request: Request, url: str = Body()):
+    if not url.startswith('http'):
+        url = f'http://{url}'
+    request.state.webhook_urls.append(url)
+    return {'url added': url}
