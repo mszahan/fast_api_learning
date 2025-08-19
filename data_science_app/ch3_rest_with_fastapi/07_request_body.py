955d53ce35afccee376a2fbb52340e62efed39b2
mszahan <mszahan1@gmail.com>
2025-08-19 18:02:25 +0600

    explored request body with and without model

---
+++ data_science_app/ch3_rest_with_fastapi/07_request_body.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Body
+
+
+app = FastAPI()
+
+
+@app.post('/users')
+async def create_user(name: str = Body(...), age: int = Body(...)):
+    return {'name': name, 'age': age}
