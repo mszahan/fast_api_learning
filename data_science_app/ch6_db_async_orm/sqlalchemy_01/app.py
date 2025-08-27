56234a49e678e4adce8d01f8d42d69ea1a6c0107
mszahan <mszahan1@gmail.com>
2025-08-27 17:40:47 +0600

    added crud operation with async database connection

--- data_science_app/ch6_db_async_orm/sqlalchemy_01/app.py
+++ data_science_app/ch6_db_async_orm/sqlalchemy_01/app.py
@@ -1,6 +1,11 @@
 import contextlib
-from fastapi import FastAPI
+from collections.abc import Sequence
+from fastapi import FastAPI, Depends, status, Query, HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy_01.database import get_async_session, create_all_tables
+from sqlalchemy_01.schemas import PostRead, PostCreate, PostPartialUpdate
+from sqlalchemy_01.models import Post
 
 
 @contextlib.asynccontextmanager
@@ -9,3 +14,66 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+
+async def pagination(
+    skip: int = Query(0, ge=0),
+    limit: int = Query(10, ge=0),
+) -> tuple[int, int]:
+    capped_limit = min(100, limit)
+    return (skip, capped_limit)
+
+
+async def get_post_or_404(
+    id: int, session: AsyncSession = Depends(get_async_session)
+) -> Post:
+    select_query = select(Post).where(Post.id == id)
+    result = await session.execute(select_query)
+    post = result.scalar_one_or_none()
+
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    return post
+
+
+@app.post('/posts', response_model=PostRead, status_code=status.HTTP_201_CREATED)
+async def create_post(post_create: PostCreate,
+                      session: AsyncSession = Depends(get_async_session)) -> Post:
+    post = Post(**post_create.model_dump())
+    session.add(post)
+    await session.commit()
+    return post
+
+
+@app.get('/posts', response_model=list[PostRead])
+async def list_posts(pagination: tuple[int, int] = Depends(pagination),
+                     session: AsyncSession = Depends(get_async_session)) -> Sequence[Post]:
+    skip, limit = pagination
+    select_query = select(Post).offset(skip).limit(limit)
+    result = await session.execute(select_query)
+    return result.scalars().all()
+
+
+@app.get('/posts/{id}', response_model=PostRead)
+async def get_post(post: Post = Depends(get_post_or_404)) -> Post:
+    return post
+
+
+@app.patch('/posts/{id}', response_model=PostRead)
+async def update_post(post_update: PostPartialUpdate,
+                      post: Post = Depends(get_post_or_404),
+                      session: AsyncSession = Depends(get_async_session)) -> Post:
+    post_update_dict = post_update.model_dump(exclude_unset=True)
+    for key, value in post_update_dict.items():
+        setattr(post, key, value)
+    session.add(post)
+    await session.commit()
+    return post
+
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(post: Post = Depends(get_post_or_404),
+                      session: AsyncSession = Depends(get_async_session)):
+    await session.delete(post)
+    await session.commit()
--- data_science_app/ch6_db_async_orm/sqlalchemy_01/database.py
+++ data_science_app/ch6_db_async_orm/sqlalchemy_01/database.py
@@ -11,7 +11,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_sessionmaker() as session:
+    async with async_session_maker() as session:
         yield session
 
 
