583358c0a4b3a1f695a80681cdc8f48814fd4392
mszahan <mszahan1@gmail.com>
2025-09-02 21:34:16 +0600

    added protected route with get_current_user dependency

--- data_science_app/ch7_authentication_security/authentication/app.py
+++ data_science_app/ch7_authentication_security/authentication/app.py
@@ -1,4 +1,5 @@
 import contextlib
+from datetime import datetime, timezone
 from fastapi import FastAPI, status, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -6,7 +7,7 @@ from sqlalchemy import exc, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import create_all_tables, get_async_session
-from models import User
+from models import User, AccessToken
 from schemas import UserRead, UserCreate
 from password import get_password_hash
 from authentication import authenticate, create_access_token
@@ -19,6 +20,23 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+async def get_current_user(
+        token: str = Depends(OAuth2PasswordBearer(tokenUrl='/token')),
+        session: AsyncSession = Depends(get_async_session)
+) -> User:
+    query = select(AccessToken).where(
+        AccessToken.access_token == token,
+        AccessToken.expiration_date >= datetime.now(tz=timezone.utc)
+
+    )
+    result = await session.execute(query)
+    access_token: AccessToken | None = result.scalar_one_or_none()
+
+    if access_token is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return access_token.user
 
 
 @app.post('/register', status_code=status.HTTP_201_CREATED, response_model=UserRead)
@@ -50,3 +68,8 @@ async def creat_token(form_data: OAuth2PasswordRequestForm = Depends(OAuth2Passw
     token = await create_access_token(user, session)
 
     return {'access_token': token.access_token, 'token_type': 'bearer'}
+
+
+@app.get('/protected-route', response_model=UserRead)
+async def protected_route(user: User = Depends(get_current_user)):
+    return user
