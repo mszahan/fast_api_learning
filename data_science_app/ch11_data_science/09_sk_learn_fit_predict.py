26ae19bc93cc6aacaf2dc6dfb5f633cd10b147d6
mszahan <mszahan1@gmail.com>
2025-09-08 17:08:49 +0600

    trained model with sklearn to recognize handwritten digits

---
+++ data_science_app/ch11_data_science/09_sk_learn_fit_predict.py
@@ -0,0 +1,26 @@
+from sklearn.datasets import load_digits
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+
+digits = load_digits()
+
+data = digits.data
+targets = digits.target
+
+
+# split into training and testing sets
+training_data, testing_data, training_targets, testing_targets = train_test_split(
+    data, targets, random_state=0)
+
+# train the model
+model = GaussianNB()
+model.fit(training_data, training_targets)
+
+# Rung the prediction with the testing set
+predicted_targets = model.predict(testing_data)
+
+# compute the accuracy
+accuracy = accuracy_score(testing_targets, predicted_targets)
+print(accuracy)
