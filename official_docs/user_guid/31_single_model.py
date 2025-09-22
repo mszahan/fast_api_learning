0e444b8841d87f7e43c2a27f522c94d1d12b10d4
mszahan <mszahan1@gmail.com>
2025-09-22 21:33:49 +0600

    fixed the single model file

--- official_docs/user_guid/31_single_model.py
+++ official_docs/user_guid/31_single_model.py
@@ -46,9 +46,36 @@ app = FastAPI(lifespan=lifespan)
 #     create_db_and_tables()
 
 
-@app.post('/heroes')
+@app.post('/heroes/')
 def create_hero(hero: Hero, session: SessionDep) -> Hero:
     session.add(hero)
     session.commit()
     session.refresh(hero)
     return hero
+
+
+@app.get('heroes/')
+def read_heroes(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100
+) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get('/heroes/{hero_id}')
+def read_hero(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail='Hero not found')
+
+
+@app.delete('/heroes/{hero_id}')
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail='Hero not found')
+    session.delete(hero)
+    session.commit()
+    return {'ok': True}
